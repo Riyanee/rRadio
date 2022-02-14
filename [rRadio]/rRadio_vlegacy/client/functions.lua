75f4f988a5f090e89d7b809ebbcf9363f3900d02
Riyanee <75226764+Riyanee@users.noreply.github.com>
2022-02-14 19:41:27 +0100

    Create functions.lua

---
+++ [rRadio]/rRadio_vlegacy/client/functions.lua
@@ -0,0 +1,29 @@
+KeyboardInput = function(entryTitle, textEntry, inputText, maxLength)
+	AddTextEntry(entryTitle, textEntry)
+	DisplayOnscreenKeyboard(1, entryTitle, '', inputText, '', '', '', maxLength)
+
+	while UpdateOnscreenKeyboard() ~= 1 and UpdateOnscreenKeyboard() ~= 2 do
+		Citizen.Wait(0)
+	end
+
+	if UpdateOnscreenKeyboard() ~= 2 then
+		local result = GetOnscreenKeyboardResult()
+		Citizen.Wait(500)
+		return result
+	else
+		Citizen.Wait(500)
+		return nil
+	end
+end
+
+CheckQuantity = function(number)
+	number = tonumber(number)
+	if type(number) == 'number' then
+		number = ESX.Math.Round(number)
+		if number > 0 then
+			return true, number
+		end
+	end
+	
+	return false, number
+end
